0f1cc17c24e6fb4e0175722b68ca7d97b8773f4b
contactbeowolf <49777059+contactbeowolf@users.noreply.github.com>
2019-04-22 17:08:19 +0530

    Update setup_tomcat.sh

--- setup_tomcat.sh
+++ setup_tomcat.sh
@@ -1,5 +1,6 @@
 #name: Installing and configuring tomcat 9 to port 8000 and add adding a default user with #username and password "tomcat"
 
+rm /opt/tomcat/
 mkdir /opt/tomcat/
 cd /opt/tomcat/
 wget https://github.com/contactbeowolf/ds-tests/raw/master/apache-tomcat-9.0.19.tar.gz
@@ -9,6 +10,7 @@ tar xvzf apache-tomcat-9.0.19.tar.gz
 
 echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64/jre" >> ~/.bashrc
 echo "export CATALINA_HOME=/opt/tomcat/apache-tomcat-9.0.19" >> ~/.bashrc
+
 cd /opt/tomcat/apache-tomcat-9.0.19/webapps/host-manager/META-INF/
 rm context.xml
 wget https://raw.githubusercontent.com/contactbeowolf/ds-tests/master/context.xml
@@ -17,14 +19,14 @@ cd /opt/tomcat/apache-tomcat-9.0.19/webapps/manager/META-INF/
 rm context.xml
 cp /opt/tomcat/apache-tomcat-9.0.19/webapps/host-manager/META-INF/context.xml /opt/tomcat/apache-tomcat-9.0.19/webapps/manager/META-INF/
 
-cd opt/tomcat/apache-tomcat-9.0.19/conf/
+cd /opt/tomcat/apache-tomcat-9.0.19/conf/
 rm tomcat-users.xml
 wget https://raw.githubusercontent.com/contactbeowolf/ds-tests/master/tomcat-users.xml
 
 cd /opt/tomcat/apache-tomcat-9.0.19/conf/
 rm server.xml
 wget https://raw.githubusercontent.com/contactbeowolf/ds-tests/master/server.xml
+
 cd /opt/tomcat/apache-tomcat-9.0.19/bin/
 source ~/.bashrc
 ./startup.sh
-echo "done"
