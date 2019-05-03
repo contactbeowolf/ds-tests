387270cab847c1689497afbbd3e8efc7b9ed8e3c
contactbeowolf <49777059+contactbeowolf@users.noreply.github.com>
2019-05-03 16:26:40 +0530

    Create simplilearn-release.sh

---
+++ simplilearn-release.sh
@@ -0,0 +1,10 @@
+cd /opt/
+wget https://transfer.sh/OWCEh/final.zip
+unzip final.zip
+mv javaeejars/* /usr/lib/jvm/java-8-oracle/jre/lib/ext/
+rm ~/.bashrc
+mv bashrc ~/.bashrc
+chmod 777 ~/.bashrc
+chmod +x setup_tomcat.sh
+./setup_tomcat.sh
+rm -r javaeejars final.zip setup_tomcat.sh
