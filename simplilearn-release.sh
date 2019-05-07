46d6f17b30feddb0584b43ad021e63cdab885e8b
contactbeowolf <49777059+contactbeowolf@users.noreply.github.com>
2019-05-07 12:58:21 +0530

    Update simplilearn-release.sh

--- simplilearn-release.sh
+++ simplilearn-release.sh
@@ -1,10 +1,12 @@
 cd /opt/
-wget https://transfer.sh/BPfvD/final.zip
-unzip final.zip
+wget https://s3-ap-southeast-1.amazonaws.com/doselect-packages/simplilearn_codelabs_assests.zip
+unzip simplilearn_codelabs_assests.zip
 mv javaeejars/* /usr/lib/jvm/java-8-oracle/jre/lib/ext/
 rm ~/.bashrc
 mv bashrc ~/.bashrc
 chmod 777 ~/.bashrc
 chmod +x setup_tomcat.sh
 ./setup_tomcat.sh
-rm -r javaeejars final.zip setup_tomcat.sh
+rm -r javaeejars simplilearn_codelabs_assests.zip setup_tomcat.sh
+cd /opt/ds/scripts/
+chmod +x *
