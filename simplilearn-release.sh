0fcee4596cca255b65e66ae1f695ab98fffc7a90
contactbeowolf <49777059+contactbeowolf@users.noreply.github.com>
2019-06-14 18:29:29 +0530

    Update simplilearn-release.sh

--- simplilearn-release.sh
+++ simplilearn-release.sh
@@ -1,5 +1,5 @@
 cd /opt/
-wget https://s3-ap-southeast-1.amazonaws.com/doselect-packages/simplilearn_codelabs_assets.zip
+wget https://doselect-packages.s3-ap-southeast-1.amazonaws.com/simplilearn_codelabs_assets.zip
 unzip simplilearn_codelabs_assets.zip
 mv javaeejars/* /usr/lib/jvm/java-8-oracle/jre/lib/ext/
 rm ~/.bashrc
